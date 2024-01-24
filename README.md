b38f0f0ce46b8d1bb143eb608223bf89b81fa69a
Romain Menke <romainmenke@gmail.com>
2024-01-24 23:20:44 +0100

    init

---
+++ README.md
@@ -0,0 +1,6 @@
+# Minimal repro for [#1914](https://github.com/postcss/postcss/issues/1914)
+
+```sh
+npm ci
+node --test
+```
---
+++ test.mjs
@@ -0,0 +1,74 @@
+import postcss32 from 'postcss-8.4.32';
+import postcss33 from 'postcss-8.4.33';
+import fs from 'fs/promises';
+import test from 'node:test';
+import assert from 'node:assert';
+
+const source = await fs.readFile('./swagger-ui.css', 'utf8');
+
+const noopPlugin = () => {
+	return {
+		postcssPlugin: 'noop-plugin',
+		Once() {
+			// do nothing
+		},
+	};
+};
+
+noopPlugin.postcss = true;
+
+test('postcss 8.4.32', async () => {
+	const result = postcss32().process(
+		source,
+		{
+			from: './swagger-ui.css',
+			map: true,
+		},
+	);
+
+	assert.equal(result.constructor.name, 'NoWorkResult');
+
+	await result;
+});
+
+test('postcss 8.4.32 - with plugin', async () => {
+	const result = postcss32([noopPlugin()]).process(
+		source,
+		{
+			from: './swagger-ui.css',
+			map: true,
+		},
+	);
+
+	assert.equal(result.constructor.name, 'LazyResult');
+
+	await result;
+});
+
+test('postcss 8.4.33', async () => {
+	const result = postcss33().process(
+		source,
+		{
+			from: './swagger-ui.css',
+			map: true,
+		},
+	);
+
+	assert.equal(result.constructor.name, 'NoWorkResult');
+
+	await result;
+});
+
+test('postcss 8.4.33 - with plugin', async () => {
+	const result = postcss33([noopPlugin()]).process(
+		source,
+		{
+			from: './swagger-ui.css',
+			map: true,
+		},
+	);
+
+	assert.equal(result.constructor.name, 'LazyResult');
+
+	await result;
+});
